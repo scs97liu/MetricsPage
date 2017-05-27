2630bd3038d825184b56312eaab635ec85191dc5
Unknown <mitchell.pynn@gmail.com>
2017-05-27 10:18:26 -0400

    Modal working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 jQuery(document).ready(function($) {
      $('.card').click(function(e) {
         e.preventDefault();
-        window.location = 'http://stackoverflow.com/';
+        alert(this.id)
+        if (this.id == "Donald") {
+            $("#demo02").click(); 
+        } else {
+            $("#demo01").click(); 
+        }
+        console.log("Clickd")
+        //window.location = 'http://stackoverflow.com/';
+    });
+
+    $(document).keyup(function(e) {
+    if (e.keyCode === 13) $("#demo01").animatedModal();     // enter
+    //if (e.keyCode === 27) $('.cancel').click();
+    if (e.keyCode === 27) {
+        $('.close-animatedModal').click(); 
+        $('.close-modal-02').click();
+    }  // esc
     });
 });
