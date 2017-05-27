2933c6669c476c1c4cd94d4ec05d903754440108
Unknown <mitchell.pynn@gmail.com>
2017-05-27 10:34:36 -0400

    Fixed up modal popup

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 jQuery(document).ready(function($) {
      $('.card').click(function(e) {
         e.preventDefault();
-        alert(this.id)
         if (this.id == "Donald") {
             $("#demo02").click(); 
         } else {
-            $("#demo01").click(); 
+            console.log("Card clicked. Do somethng here") 
         }
         console.log("Clickd")
         //window.location = 'http://stackoverflow.com/';
     });
 
     $(document).keyup(function(e) {
-    if (e.keyCode === 13) $("#demo01").animatedModal();     // enter
+    //if (e.keyCode === 13)     // enter
     //if (e.keyCode === 27) $('.cancel').click();
     if (e.keyCode === 27) {
-        $('.close-animatedModal').click(); 
         $('.close-modal-02').click();
     }  // esc
     });
